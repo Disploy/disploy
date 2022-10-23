e93b6b5f288a5b85dd35fb8f4fa276f703b66019
Tristan Camejo <contact@tristancamejo.com>
2022-10-24 09:56:38 +1100

    feat(web): impl

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Web() {
+  return (
+    <div>
+      <h1>Welcome to disploy</h1>
+      <p>
+        coming soon!{" "}
+        <a href="https://discord.gg/E3z8MDnTWn">join our discord</a>
+      </p>
+    </div>
+  );
+}
