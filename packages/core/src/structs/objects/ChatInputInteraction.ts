947595ece13d237971dae944993915df745a2f25
Tristan Camejo <contact@tristancamejo.com>
2022-10-24 16:22:27 +1100

    feat(core): temp reply

--- packages/core/src/structs/objects/ChatInputInteraction.ts
+++ packages/core/src/structs/objects/ChatInputInteraction.ts
@@ -1,7 +1,11 @@
-import type {
+import {
   APIChatInputApplicationCommandInteraction,
+  APIInteractionResponseCallbackData,
+  RESTPostAPIInteractionCallbackJSONBody,
+  Routes,
   Snowflake,
 } from "discord-api-types/v10";
+import type { Rest } from "../rest";
 import { BaseInteraction } from "./BaseInteraction";
 import { GuildMember } from "./GuildMember";
 import { User } from "./User";
@@ -27,8 +31,11 @@ export class ChatInputInteraction extends BaseInteraction {
    */
   public user!: User | null;
 
-  public constructor(raw: APIChatInputApplicationCommandInteraction) {
-    super(raw);
+  public constructor(
+    client: Rest,
+    raw: APIChatInputApplicationCommandInteraction
+  ) {
+    super(client, raw);
     this.commandId = raw.data.id;
     this.commandName = raw.data.name;
     this.member = raw.member ? new GuildMember(raw.member) : null;
@@ -37,5 +44,19 @@ export class ChatInputInteraction extends BaseInteraction {
         ? new User(raw.member.user)
         : null
       : null;
+  }
+
+  // TODO: Make this an override of BaseInteraction#reply when it's implemented.
+  // TODO: Take a response structure or json body
+  public reply(payload: APIInteractionResponseCallbackData) {
+    // TODO: Fetch reply if enabled in the arguments
+    return void this.client.post<
+      RESTPostAPIInteractionCallbackJSONBody,
+      // TODO: Replace with proper type when fetch reply is implemented
+      unknown
+    >(Routes.interactionCallback(this.id, this.token), {
+      type: 4,
+      data: payload,
+    });
   }
 }
