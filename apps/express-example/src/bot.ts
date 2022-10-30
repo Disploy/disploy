966ce998d82a6a2e6d2edbd59e48f5f61ac201b7
Tristan Camejo <contact@tristancamejo.com>
2022-10-30 15:38:49 +1100

    refactor(framework): verification

--- apps/express-example/src/bot.ts
+++ apps/express-example/src/bot.ts
@@ -7,7 +7,13 @@ import express from "express";
 const server = express();
 server.use(bodyParser.json());
 
-expressAdapter(ExampleApp, server);
+const app = new ExampleApp({
+  clientID: process.env.CLIENT_ID!,
+  publicKey: process.env.DISCORD_PUBLIC_KEY!,
+  token: process.env.DISCORD_TOKEN!,
+});
+
+expressAdapter(app, server);
 
 server.listen(4000, () => {
   console.log("🚀 Express server is running on port 4000");
---
+++ packages/framework/src/utils/runtime.ts
@@ -0,0 +1,8 @@
+const isNode =
+  typeof process !== "undefined" &&
+  process.versions != null &&
+  process.versions.node != null;
+
+export const RuntimeConstants = {
+  isNode,
+};
