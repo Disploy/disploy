c7651155a221bba89e1a01d7bc1036f93fb7af3f
TenDRILLL <sofyeer@gmail.com>
2022-10-31 12:40:23 +0200

    feat(framework): UserInteraction

---
+++ packages/framework/src/structs/UserInteraction.ts
@@ -0,0 +1,77 @@
+import type {
+    APIInteractionResponseCallbackData,
+    APIUserApplicationCommandInteraction,
+    Snowflake,
+} from "discord-api-types/v10";
+import type { App } from "../client";
+import { BaseInteraction } from "./BaseInteraction";
+import { GuildMember } from "./GuildMember";
+import { User } from "./User";
+
+export class UserInteraction extends BaseInteraction {
+    /**
+     * The ID of the command.
+     */
+    public commandId!: Snowflake;
+
+    /**
+     * The name of the command.
+     */
+    public commandName!: string;
+
+    /**
+     * The options of the interaction.
+     */
+    //public options!: UserInteractionOptions;
+
+    /**
+     * The GuildMember who invoked the interaction.
+     */
+    public member!: GuildMember | null;
+
+    /**
+     * The User who invoked the interaction.
+     */
+    public user!: User | null;
+
+    /**
+     * The targeted User's id.
+     */
+    public targetId!: Snowflake;
+
+    /*
+    * The targeted GuildMember.
+    */
+    public targetMember!: GuildMember | null;
+
+    /*
+    * The targeted User.
+    */
+    public targetUser!: User | null;
+
+    public constructor(
+        app: App,
+        public raw: APIUserApplicationCommandInteraction
+    ) {
+        super(app, raw);
+        this.commandId = raw.data.id;
+        this.commandName = raw.data.name;
+        this.member = raw.member ? new GuildMember(raw.member) : null;
+        this.user = raw.member
+            ? raw.member.user
+                ? new User(raw.member.user)
+                : null
+            : null;
+        this.targetId = raw.data.target_id;
+        this.targetMember = raw.data.resolved.members ? new GuildMember(raw.data.resolved.members[raw.data.target_id]!) : null;
+        this.targetUser = new User(raw.data.resolved.users[raw.data.target_id]!);
+        //this.options = new UserInteractionOptions(app, this);
+    }
+
+    public reply(payload: APIInteractionResponseCallbackData) {
+        return void this.app.router.emit(`${this.id}-respond`, {
+            type: 4,
+            data: payload,
+        });
+    }
+}
