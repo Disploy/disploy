6b9e56d37f32b7991bea04d2950611f8b0eaddee
Tristan Camejo <contact@tristancamejo.com>
2022-11-03 10:31:09 +1100

    feat(docs): impl

---
+++ apps/docs/docusaurus.config.js
@@ -0,0 +1,105 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Disploy",
+  tagline:
+    "Disploy is a whole ecosystem of tools to help you create your next Discord HTTP Interaction bot.",
+  url: "https://disploy.dev",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "disploy", // Usually your GitHub org/user name.
+  projectName: "disploy", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/disploy/Disploy/tree/main/apps/docs/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Disploy",
+        logo: {
+          alt: "Disploy logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "Guide/getting-started/welcome",
+            position: "left",
+            label: "Guides",
+          },
+          {
+            href: "https://github.com/disploy/Disploy",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.gg/E3z8MDnTWn",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Guides",
+                to: "/guides",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/disploy/Disploy",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Disploy. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
