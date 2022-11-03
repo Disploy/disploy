576998d90b19237866eae2431e87f0507272a241
Tristan Camejo <contact@tristancamejo.com>
2022-11-03 20:42:43 +1100

    refactor(cli): command names

---
+++ apps/cli/src/commands/build.ts
@@ -0,0 +1,42 @@
+import { spawn } from "node:child_process";
+import type { Argv, CommandModule } from "yargs";
+import { Compile } from "../lib/compiler";
+import { ProjectTools } from "../lib/ProjectTools";
+import { logger } from "../utils/logger";
+
+export const aliases: string[] = [];
+export const desc: string = "Build a project";
+
+export const builder = (yargs: Argv) => yargs.options({});
+
+export const BuildCommand: CommandModule = {
+  aliases,
+  builder,
+  command: "build",
+  async handler() {
+    logger.info("Deploying...");
+
+    const { root, prebuild, target } = await ProjectTools.resolveProject({
+      cwd: process.cwd(),
+    });
+
+    if (prebuild) {
+      logger.info("Running prebuild script...");
+      await new Promise<void>((resolve, reject) => {
+        spawn(prebuild.split(" ")[0]!, prebuild.split(" ").slice(1), {
+          cwd: process.cwd(),
+          stdio: "inherit",
+        }).on("exit", (code) => {
+          if (code === 0) {
+            resolve();
+          } else {
+            reject();
+          }
+        });
+      });
+    }
+
+    logger.info("Compiling...");
+    await Compile({ root, target });
+  },
+};
--- apps/cli/src/commands/deploy.ts
+++ apps/cli/src/commands/deploy.ts
@@ -1,8 +1,4 @@
-import { spawn } from "node:child_process";
 import type { Argv, CommandModule } from "yargs";
-import { Compile } from "../lib/compiler";
-import { ProjectTools } from "../lib/ProjectTools";
-import { logger } from "../utils/logger";
 
 export const aliases: string[] = [];
 export const desc: string = "Deploy a project";
@@ -14,29 +10,6 @@ export const DeployCommand: CommandModule = {
   builder,
   command: "deploy",
   async handler() {
-    logger.info("Deploying...");
-
-    const { root, prebuild, target } = await ProjectTools.resolveProject({
-      cwd: process.cwd(),
-    });
-
-    if (prebuild) {
-      logger.info("Running prebuild script...");
-      await new Promise<void>((resolve, reject) => {
-        spawn(prebuild.split(" ")[0]!, prebuild.split(" ").slice(1), {
-          cwd: process.cwd(),
-          stdio: "inherit",
-        }).on("exit", (code) => {
-          if (code === 0) {
-            resolve();
-          } else {
-            reject();
-          }
-        });
-      });
-    }
-
-    logger.info("Compiling...");
-    await Compile({ root, target });
+    throw new Error("Not implemented");
   },
 };
