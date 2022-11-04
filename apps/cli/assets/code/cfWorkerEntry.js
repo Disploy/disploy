ecf80590754da27e398fb4f64595b4b52319b3f8
Tristan Camejo <contact@tristancamejo.com>
2022-11-04 14:32:06 +1100

    Update apps/cli/assets/code/cfWorkerEntry.js
    Co-authored-by: Suneet Tipirneni <77477100+suneettipirneni@users.noreply.github.com>

--- apps/cli/assets/code/cfWorkerEntry.js
+++ apps/cli/assets/code/cfWorkerEntry.js
@@ -19,20 +19,17 @@ function createCloudflareAdapter(app) {
     const { status, headers, body } = payload.serialized;
     return new Response(JSON.stringify(body), {
       status,
-      headers: (() => {
-        const headersObj = {
-          "content-type": "application/json",
-        };
-        for (const [key, value] of Object.entries(headers)) {
-          if (typeof value === "string") {
-            headersObj[key] = value;
-          }
-          if (Array.isArray(value)) {
-            headersObj[key] = value.join(",");
-          }
+      headers: Object.entries(headers).reduce<Record<string, string>>((acc, [key, value]) => {
+        if (typeof value === "string") {
+          acc[key] = value;
         }
-        return headersObj;
-      })(),
+        
+        if (Array.isArray(value)) {
+          acc[key] = value.join(",");
+        }
+          
+        return acc;
+      }),
     });
   };
 }
