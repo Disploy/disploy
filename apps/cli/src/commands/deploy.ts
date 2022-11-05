d7389ec7a92180e2d9b1253af820de0ba5cb3371
Tristan Camejo <contact@tristancamejo.com>
2022-11-05 18:16:52 +1100

    feat(cli): deploy command

---
+++ apps/cli/src/commands/deploy.ts
@@ -0,0 +1,53 @@
+import * as color from 'colorette';
+import ora from 'ora';
+import type { Argv, CommandModule } from 'yargs';
+import { ProjectTools } from '../lib/ProjectTools';
+import { UserError } from '../lib/UserError';
+import { WranglerWrapper } from '../lib/WranglerWrapper';
+import { logger } from '../utils/logger';
+import { BuildApp } from './common/build';
+
+export const aliases: string[] = [];
+export const desc: string = 'Build a project';
+
+export const builder = (yargs: Argv) => yargs.options({});
+
+export const DeployCommand: CommandModule = {
+	aliases,
+	builder,
+	command: 'deploy',
+	async handler() {
+		const conf = await ProjectTools.resolveProject({
+			cwd: process.cwd(),
+		});
+		const entry = await BuildApp({
+			skipPrebuild: true,
+		});
+
+		switch (conf.target.type) {
+			case 'cloudflare': {
+				const wrangler = new WranglerWrapper(conf, entry);
+
+				const spinner = ora('Deploying to Cloudflare Workers').start();
+
+				await wrangler.publish();
+
+				spinner.succeed('Deployed to Cloudflare Workers');
+
+				logger.info(
+					[
+						"You should visit the Cloudflare Workers dashboard to add your environment variables if you haven't already.",
+						`${color.gray('CLIENT_ID')} Your Discord application's client ID`,
+						`${color.gray('PUBLIC_KEY')} Your Discord application's public key`,
+						`${color.gray('TOKEN')} Your Discord application's bot token`,
+					].join('\n'),
+				);
+				break;
+			}
+
+			default: {
+				throw new UserError(`Unsupported deploy target type: ${conf.target.type}`);
+			}
+		}
+	},
+};
