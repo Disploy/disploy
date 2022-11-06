4bab44094dc5be4a37a43d83cb740589cf86a191
Tristan Camejo <contact@tristancamejo.com>
2022-11-06 16:38:51 +1100

    update(Guild): afk props

--- packages/framework/src/structs/Guild.ts
+++ packages/framework/src/structs/Guild.ts
@@ -1,6 +1,8 @@
 import type { APIGuild, Snowflake } from 'discord-api-types/v10';
 import type { App } from '../client';
 import { Base } from './Base';
+import { Channel } from './Channel';
+import { ToBeFetched } from './ToBeFetched';
 
 export class Guild extends Base {
 	/**
@@ -18,10 +20,31 @@ export class Guild extends Base {
 	 */
 	public name!: string;
 
+	/**
+	 * The AFK channel of the guild.
+	 */
+	public afkChannel!: ToBeFetched<Channel> | null;
+
+	/**
+	 * The AFK channel's ID of the guild.
+	 */
+	public afkChannelId!: Snowflake | null;
+
+	/**
+	 * The time in seconds a user has to be AFK before being moved to the AFK channel.
+	 */
+	public afkTimeout!: number;
+
 	public constructor(app: App, raw: APIGuild) {
 		super(app);
 		this.id = raw.id;
 		this.ownerId = raw.owner_id;
 		this.name = raw.name;
+
+		this.afkChannel = raw.afk_channel_id
+			? new ToBeFetched(this, Channel, () => app.rest.get(`/channels/${raw.afk_channel_id}`))
+			: null;
+		this.afkChannelId = raw.afk_channel_id;
+		this.afkTimeout = raw.afk_timeout;
 	}
 }
