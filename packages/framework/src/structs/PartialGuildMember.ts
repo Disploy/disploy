ab40ed75c2e4fd6caed56b07f854e8c0180f6c38
Tristan Camejo <contact@tristancamejo.com>
2022-11-06 12:30:03 +1100

    fix(PartialGuildMember): camelcase

--- packages/framework/src/structs/PartialGuildMember.ts
+++ packages/framework/src/structs/PartialGuildMember.ts
@@ -1,48 +1,48 @@
-import type {APIGuildMember, APIInteractionDataResolvedGuildMember, Snowflake} from "discord-api-types/v10";
+import type { APIGuildMember, APIInteractionDataResolvedGuildMember, Snowflake } from 'discord-api-types/v10';
 
 export class PartialGuildMember {
-    /**
-     * The nickname of the member.
-     */
-    public nickname!: string | null;
+	/**
+	 * The nickname of the member.
+	 */
+	public nickname!: string | null;
 
-    /**
-    * The member's guild avatar hash.
-    */
-    public avatar!: string | null;
+	/**
+	 * The member's guild avatar hash.
+	 */
+	public avatar!: string | null;
 
-    /**
-    * Array of role object ids.
-    */
-    public roles!: Snowflake[];
+	/**
+	 * Array of role object ids.
+	 */
+	public roles!: Snowflake[];
 
-    /**
-     * When the user joined the guild.
-     */
-    public joined_at!: string;
+	/**
+	 * When the user joined the guild.
+	 */
+	public joinedAt!: string;
 
-    /**
-     * When the user started boosting the guild.
-     */
-    public premium_since!: string | null;
+	/**
+	 * When the user started boosting the guild.
+	 */
+	public premiumSince!: string | null;
 
-    /**
-     * Whether the user has not yet passed the guild's Membership Screening requirements.
-     */
-    public pending!: boolean | null;
+	/**
+	 * Whether the user has not yet passed the guild's Membership Screening requirements.
+	 */
+	public pending!: boolean | null;
 
-    /**
-     * Timestamp of when the time out will be removed; until then, they cannot interact with the guild.
-     */
-    public communication_disabled_until!: boolean | null;
+	/**
+	 * Timestamp of when the time out will be removed; until then, they cannot interact with the guild.
+	 */
+	public communication_disabled_until!: boolean | null;
 
-    public constructor(raw: APIGuildMember | APIInteractionDataResolvedGuildMember) {
-        this.nickname = raw.nick ?? null;
-        this.avatar = raw.avatar ?? null;
-        this.roles = raw.roles;
-        this.joined_at = raw.joined_at;
-        this.premium_since = raw.premium_since ?? null;
-        this.pending = raw.pending ?? null;
-        this.communication_disabled_until = Boolean(raw.communication_disabled_until) ?? null;
-    }
+	public constructor(raw: APIGuildMember | APIInteractionDataResolvedGuildMember) {
+		this.nickname = raw.nick ?? null;
+		this.avatar = raw.avatar ?? null;
+		this.roles = raw.roles;
+		this.joinedAt = raw.joined_at;
+		this.premiumSince = raw.premium_since ?? null;
+		this.pending = raw.pending ?? null;
+		this.communication_disabled_until = Boolean(raw.communication_disabled_until) ?? null;
+	}
 }
