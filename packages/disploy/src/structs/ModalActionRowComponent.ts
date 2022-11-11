e38dfd1fccc6839747af170982d376f4647f08a4
TenDRILLL <sofyeer@gmail.com>
2022-11-11 18:28:21 +0200

    refactor(structs): move TextInputComponent methods and properties out

--- packages/disploy/src/structs/ModalActionRowComponent.ts
+++ packages/disploy/src/structs/ModalActionRowComponent.ts
@@ -1,45 +1,27 @@
 import {ActionRowComponent} from "./ActionRowComponent";
 import type {App} from "../client";
-import {ComponentType, TextInputStyle} from "discord-api-types/v10";
 
 export class ModalActionRowComponent extends ActionRowComponent {
-    public style: TextInputStyle | undefined;
-    public label: string | undefined;
-    public minLength: number | null = null;
-    public maxLength: number | null = null;
-    public required: boolean | null = null;
-    public value: string | null = null;
-    public placeholder: string | null = null;
-
-    setStyle(style: TextInputStyle){
-        this.style = style;
-    }
-
-    setLabel(label: string){
-        this.label = label;
-    }
-
-    setMinLength(length: number){
-        this.minLength = length;
-    }
-
-    setMaxLength(length: number){
-        this.maxLength = length;
-    }
+    public required?: boolean;
+    public value?: string;
+    public placeholder?: string;
 
     setRequired(required: boolean){
         this.required = required;
+        return this;
     }
 
     setValue(value: string){
         this.value = value;
+        return this;
     }
 
     setPlaceholder(placeholder: string){
         this.placeholder = placeholder;
+        return this;
     }
 
     constructor(app: App) {
-        super(app, ComponentType.TextInput);
+        super(app);
     }
 }
