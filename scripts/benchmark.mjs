ee2d98038a359fe2274b8d72dc206aaa37b0e957
Tristan Camejo <contact@tristancamejo.com>
2022-11-13 23:23:29 +1100

    fix: full path

--- scripts/benchmark.mjs
+++ scripts/benchmark.mjs
@@ -5,11 +5,12 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const server = spawn('yarn', ['disploy', 'test-server'], {
+const server = spawn('yarn', ['workspace', '@disploy/example', 'test-server'], {
 	cwd: join(__dirname, '..', 'apps', 'example'),
 });
 
 server.stdout.on('data', (data) => {
+	console.log(`server: ${data}`);
 	if (data.includes('Server Ready!')) {
 		const benchmark = spawn('yarn', ['disbench', 'internal', 'benchmark', '-u', 'http://localhost:5002/interactions'], {
 			cwd: join(__dirname, '..', 'apps', 'example'),
