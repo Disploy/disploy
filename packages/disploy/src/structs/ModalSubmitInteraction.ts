ef5d2dc3a7a04e1c1652767ad2f7c546d180924d
TenDRILLL <sofyeer@gmail.com>
2022-11-14 21:17:09 +0200

    feat(structs): ModalSubmitInteraction#reply

--- packages/disploy/src/structs/ModalSubmitInteraction.ts
+++ packages/disploy/src/structs/ModalSubmitInteraction.ts
@@ -3,10 +3,19 @@ import type { APIModalSubmitInteraction } from 'discord-api-types/v10';
 import type { App } from '../client';
 import { ActionRow } from './ActionRow';
 import { ModalSubmitComponent } from './ModalSubmitComponent';
+import {APIInteractionResponseCallbackData, InteractionResponseType} from "discord-api-types/v10";
+import {RouterEvents} from "../router";
 
 export class ModalSubmitInteraction extends BaseInteraction {
 	public customId: string;
 	public components: ActionRow[];
+
+	public reply(payload: APIInteractionResponseCallbackData) {
+		return void this.app.router.emit(RouterEvents.Respond(this.id), {
+			type: InteractionResponseType.ChannelMessageWithSource,
+			data: payload,
+		});
+	}
 
 	public constructor(app: App, raw: APIModalSubmitInteraction) {
 		super(app, raw);
