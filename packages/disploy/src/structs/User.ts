69340c1295a7c4bb0513b16e75915d68edfa1f15
Tristan Camejo <contact@tristancamejo.com>
2022-11-22 20:33:11 +1100

    refactor(structs): User#tag

--- packages/disploy/src/structs/User.ts
+++ packages/disploy/src/structs/User.ts
@@ -5,23 +5,34 @@ import { Base } from './Base';
 export class User extends Base {
 	/**
 	 * The ID of the user.
+	 * @example '97470053615673344'
 	 */
 	public id!: Snowflake;
 
 	/**
-	 * The name of the user.
+	 * The username of the user.
+	 * @example 'Disploy'
 	 */
-	public name!: string;
+	public username!: string;
+
+	/**
+	 * The tag of the user.
+	 * @example 'tristan#0005'
+	 */
+	public get tag(): `${string}#${string}` {
+		return `${this.username}#${this.discriminator}`;
+	}
 
 	/**
 	 * The discriminator of the user.
+	 * @example '0005'
 	 */
 	public discriminator!: string;
 
 	public constructor(app: App, raw: APIUser) {
 		super(app);
 		this.id = raw.id;
-		this.name = raw.username;
+		this.username = raw.username;
 		this.discriminator = raw.discriminator;
 	}
 }
