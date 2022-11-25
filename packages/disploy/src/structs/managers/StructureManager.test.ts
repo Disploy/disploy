054c4b3076d0e444e39cc06c49936e992aeac111
Tristan Camejo <contact@tristancamejo.com>
2022-11-25 13:56:03 +1100

    feat(tests): StructureManager

---
+++ packages/disploy/src/structs/managers/StructureManager.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, test } from '@jest/globals';
+import { Base } from '../Base';
+import { StructureManager } from './StructureManager';
+
+interface ExampleData {
+	id: string;
+	name: string;
+}
+
+async function fetchExampleData(id: string): Promise<ExampleData> {
+	return {
+		id,
+		name: `Example ${id}`,
+	};
+}
+
+class ExampleClass extends Base {
+	public id: string;
+	public name: string;
+
+	public constructor(_: null, raw: ExampleData) {
+		super(null!);
+		this.id = raw.id;
+		this.name = raw.name;
+	}
+}
+
+const ExampleStructureManager = new StructureManager<ExampleClass>(null!, ExampleClass, (id) => fetchExampleData(id));
+
+describe('StructureManager', () => {
+	test('Example fetch', async () => {
+		const tristan = await ExampleStructureManager.fetch('tristan');
+
+		expect(tristan).toBeInstanceOf(ExampleClass);
+		expect(tristan.id).toBe('tristan');
+		expect(tristan.name).toBe('Example tristan');
+	});
+});
