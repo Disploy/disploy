187896099d839fb2f443554ac9ce00326c958530
Tristan Camejo <contact@tristancamejo.com>
2022-11-27 17:42:48 +1100

    feat(cli): skip-prebuild

--- packages/disploy/cli/src/commands/build.ts
+++ packages/disploy/cli/src/commands/build.ts
@@ -4,13 +4,21 @@ import { BuildApp } from './common/build';
 export const aliases: string[] = [];
 export const desc: string = 'Build a project';
 
-export const builder = (yargs: Argv) => yargs.options({});
+export const builder = (yargs: Argv) =>
+	yargs.options('skip-prebuild', {
+		type: 'boolean',
+		alias: 'sp',
+		description: 'Skip the prebuild script',
+		default: false,
+	});
 
-export const BuildCommand: CommandModule = {
+export const BuildCommand: CommandModule<{}, { 'skip-prebuild': boolean }> = {
 	aliases,
 	builder,
 	command: 'build',
-	async handler() {
-		await BuildApp();
+	async handler(opts) {
+		await BuildApp({
+			skipPrebuild: opts.skipPrebuild,
+		});
 	},
 };
