4f798fdcde21d70daaa0dc23fef1ce6edf2b0fd6
Tristan Camejo <contact@tristancamejo.com>
2022-12-02 18:46:07 +1100

    chore: chunking

--- packages/disploy/src/structs/ChatInputInteractionResolvedOptions.ts
+++ packages/disploy/src/structs/ChatInputInteractionResolvedOptions.ts
@@ -8,48 +8,54 @@ import { User } from './User';
 
 export class ChatInputInteractionResolvedOptions extends Base {
 	/**
-	* The interaction the resolved options belong to.
-	*/
+	 * The interaction the resolved options belong to.
+	 */
 	public interaction: ChatInputInteraction;
-	
+
 	/**
-	* The resolved members.
-	*/
+	 * The resolved members.
+	 */
 	public members: Map<Snowflake, PartialGuildMember>;
-	
+
 	/**
-	* The resolved users.
-	*/
+	 * The resolved users.
+	 */
 	public users: Map<Snowflake, User>;
-	
+
 	/**
-	* The resolved channels.
-	*/
+	 * The resolved channels.
+	 */
 	public channels: Map<Snowflake, BaseChannel>;
-	
+
 	constructor(interaction: ChatInputInteraction) {
 		super(interaction.app);
 		this.interaction = interaction;
 		this.members = new Map();
 		this.users = new Map();
 		this.channels = new Map();
+
 		if (!interaction.raw.data.resolved) return;
+
 		const { users, members, channels } = interaction.raw.data.resolved;
+
 		if (users) {
 			for (const user in users) {
 				this.users.set(user, new User(this.interaction.app, users[user]!));
 			}
 		}
+
 		if (channels) {
 			for (const channel in channels) {
 				this.channels.set(channel, new PartialChannel(this.interaction.app, channels[channel]!));
 			}
 		}
+
 		if (members) {
 			for (const member in members) {
 				if (!this.users.has(member)) {
 					this.interaction.app.logger.debug(`An invalid member with the id ${member} was provided`);
 				}
+
 				this.members.set(member, new PartialGuildMember(this.interaction.app, members[member]!));
 			}
 		}
