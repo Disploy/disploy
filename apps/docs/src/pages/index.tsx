48c6c5d15d58ffe9c48d3f3b27099eaa5d4c7caf
Tristan Camejo <contact@tristancamejo.com>
2022-12-05 08:50:38 +1100

    chore(docs): new tagline

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home(): JSX.Element {
 	return (
 		<Layout
 			title="Disploy"
-			description="Next.js for Discord applications - The ecosystem to help you create your Discord bot. Disploy is a framework for building HTTP Discord bots with ease. It's designed to make it easy to build, test and deploy Discord bots."
+			description="Flexible router for building HTTP interaction-based Discord bots with ease."
 		>
 			<HomepageHeader />
 		</Layout>
