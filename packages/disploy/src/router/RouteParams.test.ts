55a9bf093c2d01bb7c910342f0408a973da5c320
Tristan Camejo <contact@tristancamejo.com>
2022-12-29 14:46:34 +1100

    feat(tests): multiple route params

--- packages/disploy/src/router/RouteParams.test.ts
+++ packages/disploy/src/router/RouteParams.test.ts
@@ -9,4 +9,13 @@ describe('RouteParams', () => {
 
 		expect(params.getParam('id')).toBe('123');
 	});
+
+	test('Parse(test-:id-:id2, test-123-456) -> id = 123, id2 = 456', () => {
+		const template = 'test-:id-:id2';
+		const data = 'test-123-456';
+		const params = new RouteParams(null, template, data);
+
+		expect(params.getParam('id')).toBe('123');
+		expect(params.getParam('id2')).toBe('456');
+	});
 });
