b207befded52f0ca226f19e30e5bd8ac4f86ca2f
Tristan Camejo <contact@tristancamejo.com>
2022-12-29 15:09:10 +1100

    fix(nextAdapter): randId

--- packages/disploy/src/adapters/nextAdapter.ts
+++ packages/disploy/src/adapters/nextAdapter.ts
@@ -2,11 +2,19 @@ import type { NextApiRequest, NextApiResponse } from 'next/types';
 import type { App } from '../client';
 import type { TRequest } from '../http';
 
-export function createNextAdapter(app: App, randId?: string) {
+export function createNextAdapter(app: App) {
 	return async function (req: NextApiRequest, res: NextApiResponse) {
 		if (req.method !== 'POST') {
 			res.status(405);
 			res.json({ error: 'Method not allowed' });
+			return;
+		}
+
+		const randId = req.query.randId;
+
+		if (randId && typeof randId !== 'string') {
+			res.status(400);
+			res.json({ error: 'Invalid randId' });
 			return;
 		}
 
